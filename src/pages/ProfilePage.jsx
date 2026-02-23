588aef0b0e4c9beff7e8af9bf06cec6d080b1309
raffaelebaiocco06-ux <raffaele.baiocco06@gmail.com>
2026-02-23 16:28:01 +0100

    sfondo...

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -12,7 +12,7 @@ import Organization from "../components/Organizations";
 import Interest from "../components/Interest";
 import MyFooter from "../components/MyFooter";
 import RightSideBar from "../components/RightSideBar";
-import { Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const ProfilePage = () => {
   return (
@@ -21,7 +21,7 @@ const ProfilePage = () => {
         <NavBar />
         {/* navbar giulia */}
       </header>
-      <main className="container sfondo">
+      <main className="sfondo">
         <Row>
           <Col xs={12} md={9}>
             <FirstSection />
