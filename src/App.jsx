a4af1a3dbf6f0159e07feec484e64971620c378c
Giulia Creps <giulia.creps2@gmail.com>
2026-02-23 18:03:04 +0100

    topnavbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./App.css"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import { Container, Row, Col, CardGroup, Card, Spinner, Alert, Button, Image, Form, ListGroup, Navbar, NavDropdown, Nav } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import TopNavbar from "./components/TopNavbar";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Container, Row, Col, CardGroup, Card, Spinner, Alert, Button, Image, Form, ListGroup, Navbar, NavDropdown, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <>
-      
+      <TopNavbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
