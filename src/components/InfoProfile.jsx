2fffe46596c5df0580c7e52feb14d132d376e7fb
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-02-23 18:38:22 +0100

    info done

--- src/components/InfoProfile.jsx
+++ src/components/InfoProfile.jsx
@@ -1,4 +1,19 @@
+import { FaPen } from "react-icons/fa";
+
 const InfoProfile = () => {
-  return;
+  return (
+    <>
+      <section className=" bg-light border border-1 border-secondary-subtle rounded-3 container pt-2">
+        <div className="d-flex justify-content-between align-items-center">
+          <p className="m-0 fs-5 fw-semibold">Informazioni</p>
+          <FaPen />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, sit id asperiores, tempora delectus eligendi laudantium quis maiores perferendis
+          eveniet dolorum rem voluptatibus! Dolorem, sapiente. Omnis a animi esse ullam.
+        </p>
+      </section>
+    </>
+  );
 };
 export default InfoProfile;
