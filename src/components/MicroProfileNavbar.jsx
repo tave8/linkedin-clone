75774867edeb1fd5c86de51241dd2ab5bc0c11e5
Giulia Creps <giulia.creps2@gmail.com>
2026-02-24 15:38:19 +0100

    .

--- src/components/MicroProfileNavbar.jsx
+++ src/components/MicroProfileNavbar.jsx
@@ -3,7 +3,7 @@ function MicroProfileNavbar() {
     <div className="d-flex flex-column p-4">
       <img src="/logo-linkedin.png" className="mb-3 rounded-circle" width={80} />
       <h4>Nome Utente</h4>
-      <h6>Desacrizione Lavoro</h6>
+      <h6>Descrizione Lavoro</h6>
       <p className="text-secondary">Città</p>
     </div>
   );
