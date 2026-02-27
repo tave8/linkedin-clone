dab25bfc3fa46a939a9d662972733b80ab14ad91
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-02-27 11:37:48 +0100

    redux store: messages tab is open

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import myProfileReducer from "../reducers/myProfileReducer"
 import myProfilesReducer from "../reducers/myProfilesReducer"
+import messagesTabReducer from "../reducers/messagesTabReducer"
 
 const bigReducer = combineReducers({
   myProfile: myProfileReducer,
   myProfiles: myProfilesReducer,
+  messagesTab: messagesTabReducer,
 })
 
 const store = configureStore({
